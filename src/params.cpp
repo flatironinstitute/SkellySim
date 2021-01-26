ac2857d4acbf8a8ea9dee46d6af76bf196e8ce59
Robert Blackwell <rblackwell@flatironinstitute.org>
2021-01-26 09:02:28 -0500

    Change default timestep

--- src/params.cpp
+++ src/params.cpp
@@ -3,7 +3,7 @@
 Params::Params(toml::table *param_table) {
     toml::table &pt = *param_table;
     eta = pt["eta"].value_or(1.0);
-    dt = pt["dt"].value_or(0.005);
+    dt = pt["dt"].value_or(1E-4);
     gmres_tol = pt["gmres_tol"].value_or(1E-12);
 
     shell_precompute_file = pt["shell_precompute_file"].value_or("");
