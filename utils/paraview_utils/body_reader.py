f0906d8ace4dbd7f88f2b44f779cb8b2f3596294
Robert Blackwell <rblackwell@flatironinstitute.org>
2021-07-12 15:34:40 -0400

    Add toml reader to load simulation metadata

--- utils/paraview_utils/body_reader.py
+++ utils/paraview_utils/body_reader.py
@@ -1,5 +1,6 @@
 import vtk
 from trajectory_utility import load_frame
+import toml
 
 outInfo = self.GetOutputInformation(0)
 
@@ -8,7 +9,6 @@ if outInfo.Has(vtk.vtkStreamingDemandDrivenPipeline.UPDATE_TIME_STEP()):
 else:
     time = 0
 
-
 timestep = len(self.times) - 1
 for i in range(len(self.times) - 1):
     if time < self.times[i+1] and time >= self.times[i]:
@@ -16,12 +16,15 @@ for i in range(len(self.times) - 1):
 
 frame = load_frame(self.fhs, self.fpos, timestep)
 
+with open(toml_file) as f:
+    skelly_config = toml.load(f)
+
 mb = vtk.vtkMultiBlockDataSet()
 offset = 0
-for body in frame["bodies"]:
+for i, body in enumerate(frame["bodies"]):
     position = body["position_"][3:]
     s = vtk.vtkSphereSource()
-    s.SetRadius(0.5)
+    s.SetRadius(skelly_config["bodies"][i]['radius'])
     s.SetCenter(position)
     s.SetThetaResolution(32)
     s.SetPhiResolution(32)
