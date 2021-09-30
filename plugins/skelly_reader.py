25584cc86f618413cc28e90b19ed1aab2a906084
Robert Blackwell <rblackwell@flatironinstitute.org>
2021-09-30 14:07:27 -0400

    Add velocity field reader to paraview plugin

--- plugins/skelly_reader.py
+++ plugins/skelly_reader.py
@@ -4,21 +4,83 @@ from vtkmodules.vtkCommonDataModel import vtkDataSet, vtkPolyData, vtkMultiBlock
 from vtkmodules.util.vtkAlgorithm import VTKPythonAlgorithmBase
 from paraview.util.vtkAlgorithm import smproxy, smproperty, smdomain
 
+class DesyncError(Exception):
+    pass
+
+def get_frame_info(filenames):
+    import msgpack
+    if not filenames:
+        return [], [], []
+
+    unpackers = []
+    fhs = []
+    times = []
+    for filename in filenames:
+        f = open(filename, "rb")
+        fhs.append(f)
+        unpackers.append(msgpack.Unpacker(f, raw=False))
+
+    fpos = [[] for i in range(len(filenames))]
+    while True:
+        try:
+            for i in range(len(unpackers)):
+                fpos[i].append(unpackers[i].tell())
+                if i == 0:
+                    n_keys = unpackers[i].read_map_header()
+                    for ikey in range(n_keys):
+                        key = unpackers[i].unpack()
+                        if key == 'time':
+                            times.append(unpackers[i].unpack())
+                        else:
+                            unpackers[i].skip()
+                else:
+                    unpackers[i].skip()
+
+        except msgpack.exceptions.OutOfData:
+            fpos[0].pop()
+            break
+
+    return fhs, fpos, times
+
+
 class SkellyReader(VTKPythonAlgorithmBase):
-    def __init__(self, outputType, static=False):
+    def __init__(self, outputType, static=False, filepattern="skelly_sim.out.*"):
         VTKPythonAlgorithmBase.__init__(self, nInputPorts=0, nOutputPorts=1, outputType=outputType)
 
+        import msgpack
+        self.Unpacker = msgpack.Unpacker
+
         if static:
-            self.times = []
+            self.times = None
         else:
-            from skelly_sim.paraview_utils.trajectory_utility import get_frame_info
-            self.fhs, self.fpos, self.times = get_frame_info(sorted(Path('.').glob('skelly_sim.out.*')))
+            self.fhs, self.fpos, self.times = get_frame_info(sorted(Path('.').glob(filepattern)))
 
         # FIXME: Get this from user
         toml_file = sorted(Path('.').glob('*.toml'))[0]
         import toml
         with open(toml_file) as f:
             self.skelly_config = toml.load(f)
+
+    def _load_frame(self, index):
+        data = []
+        for i in range(len(fhs)):
+            fhs[i].seek(self.fpos[i][index])
+            data.append(self.Unpacker(self.fhs[i], raw=False).unpack())
+
+        time = data[0]["time"]
+        dt = data[0]["dt"]
+        fibers = []
+        bodies = []
+        for el in data:
+            if el["time"] != time or el["dt"] != dt:
+                raise DesyncError
+            fibers.extend(el["fibers"][0])
+            el.pop("fibers")
+
+        data[0]["fibers"] = fibers
+        data[0]["bodies"] = data[0]["bodies"][0]
+
+        return data[0]
 
     def _get_timesteps(self):
         return self.times
@@ -77,13 +139,9 @@ class FiberReader(SkellyReader):
         return self._get_timesteps()
 
     def RequestData(self, request, inInfo, outInfo):
-        from vtkmodules.vtkCommonDataModel import vtkPolyData
-        from skelly_sim.paraview_utils.trajectory_utility import load_frame
         info = outInfo.GetInformationObject(0)
-
         timestep = self._get_update_timestep(info)
-
-        frame = load_frame(self.fhs, self.fpos, timestep)
+        frame = self._load_frame(timestep)
 
         pts = vtk.vtkPoints()
         lines = vtk.vtkCellArray()
@@ -118,12 +176,9 @@ class BodyReader(SkellyReader):
         return self._get_timesteps()
 
     def RequestData(self, request, inInfo, outInfo):
-        from skelly_sim.paraview_utils.trajectory_utility import load_frame
         info = outInfo.GetInformationObject(0)
-
         timestep = self._get_update_timestep(info)
-
-        frame = load_frame(self.fhs, self.fpos, timestep)
+        frame = self._load_frame(self.fhs, self.fpos)
 
         mb = vtk.vtkMultiBlockDataSet()
         offset = 0
@@ -169,5 +224,64 @@ class PeripheryReader(SkellyReader):
         self.source.Update()
         output = vtk.vtkPolyData.GetData(outInfo, 0)
         output.ShallowCopy(self.source.GetOutput())
+
+        return 1
+
+
+@smproxy.source(label="Velocity Field Reader")
+class SkellyFieldReader(SkellyReader):
+    def __init__(self):
+        SkellyReader.__init__(self, outputType='vtkPolyData', filepattern="skelly_sim.vf.*")
+
+    @smproperty.doublevector(name="TimestepValues", information_only="1", si_class="vtkSITimeStepsProperty")
+    def GetTimestepValues(self):
+        return self._get_timesteps()
+
+    def _load_frame(self, index):
+        data = []
+        for i in range(len(self.fhs)):
+            self.fhs[i].seek(self.fpos[i][index])
+            data.append(self.Unpacker(self.fhs[i], raw=False).unpack())
+
+        return data
+
+    def RequestData(self, request, inInfo, outInfo):
+        import numpy as np
+        info = outInfo.GetInformationObject(0)
+        timestep = self._get_update_timestep(info)
+        frame = self._load_frame(timestep)
+
+        pts = vtk.vtkPoints()
+        npts = int(sum([data["x_grid"][2] for data in frame]))
+        vertices = vtk.vtkPolyData()
+
+        velocities = vtk.vtkDoubleArray()
+        velocities.SetName("velocities")
+        velocities.SetNumberOfComponents(3)
+        velocities.SetNumberOfTuples(npts)
+
+        magnitudes = vtk.vtkDoubleArray()
+        magnitudes.SetName("magnitudes")
+        magnitudes.SetNumberOfComponents(0)
+        magnitudes.SetNumberOfValues(npts)
+
+        offset = 0
+        for data in frame:
+            n_points_local = data["x_grid"][2]
+            x_grid = data["x_grid"][3:]
+            v_grid = data["v_grid"][3:]
+            for i in range(n_points_local):
+                pts.InsertPoint(offset, x_grid[3 * i : 3* (i + 1)])
+                # verts.InsertNextCell(1, [offset])
+                mag = np.linalg.norm(np.array(v_grid[3*i:3*(i+1)]))
+                magnitudes.SetValue(offset, mag)
+                velocities.SetTuple(offset, v_grid[3 * i : 3* (i + 1)])
+                offset += 1
+
+
+        output = vtkPolyData.GetData(outInfo, 0)
+        output.SetPoints(pts)
+        output.GetPointData().AddArray(velocities)
+        output.GetPointData().AddArray(magnitudes)
 
         return 1
