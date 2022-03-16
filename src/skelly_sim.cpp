e4e91d6bc45acc7ab91e465a8263d1541630fb10
Robert Blackwell <rblackwell@flatironinstitute.org>
2022-03-16 10:58:15 -0400

    Add safety flag --overwrite to prevent you from killing your sims

--- src/skelly_sim.cpp
+++ src/skelly_sim.cpp
@@ -3,6 +3,7 @@
 #include <system.hpp>
 
 #include <spdlog/spdlog.h>
+#include <filesystem>
 #include <stdexcept>
 
 #include <Teuchos_CommandLineProcessor.hpp>
@@ -12,17 +13,25 @@ int main(int argc, char *argv[]) {
     int thread_level;
     MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &thread_level);
 
-    std::string config_file;
-    int resume_flag = false;
+    std::string config_file = "skelly_config.toml";
+    bool resume_flag = false;
+    bool overwrite_flag = false;
     Teuchos::CommandLineProcessor cmdp(false, true);
     cmdp.setOption("config-file", &config_file, "TOML input file.");
-    cmdp.setOption("resume-flag", &resume_flag, "Flag to resume simulation.");
+    cmdp.setOption("resume", "no-resume", &resume_flag, "Supply to resume simulation.");
+    cmdp.setOption("overwrite", "no-overwrite", &overwrite_flag, "Supply to overwrite existing simulation.");
     if (cmdp.parse(argc, argv) != Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL) {
         MPI_Finalize();
         return EXIT_FAILURE;
     }
 
     try {
+        if (resume_flag && overwrite_flag)
+            throw std::runtime_error("Can't resume and overwrite simultaneously.");
+        namespace fs = std::filesystem;
+        if (!overwrite_flag && (fs::exists(fs::path{"skelly_sim.out"}) || fs::exists(fs::path{"skelly_sim.vf"}))
+            throw std::runtime_error("Existing trajectory detected. Supply --overwrite flag to overwrite.");
+
         System::init(config_file, resume_flag);
         System::run();
     } catch (const std::runtime_error &e) {
--- src/system.cpp
+++ src/system.cpp
@@ -4,6 +4,7 @@
 
 #include <fstream>
 #include <sstream>
+#include <stdexcept>
 #include <string>
 #include <unordered_map>
 
