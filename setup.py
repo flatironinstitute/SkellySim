c2f913f769e0d8cee5443c618d0521eb8c0ef931
Robert Blackwell <rblackwell@flatironinstitute.org>
2022-04-12 12:01:41 -0400

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='skelly_sim',
-    version='0.9.7',
+    version='0.9.8',
     description='Simulate cytoskeletal systems with full hydrodynamics',
     long_description=long_description,
     url='https://github.com/flatironinstitute/skelly_sim/',
