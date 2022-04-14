9afce05fef811d1459ca2208c353cbfff57002db
Robert Blackwell <rblackwell@flatironinstitute.org>
2022-04-14 12:37:31 -0400

    post: add internal fiber forces for velocity field calculation

--- src/system.cpp
+++ src/system.cpp
@@ -1038,6 +1038,9 @@ void VelocityField::compute() {
         }
     }
 
+    if (properties.time >= params_.implicit_motor_activation_delay)
+        f_on_fibers += fc_.generate_constant_force();
+
     // FIXME: This is likely wrong, but more right than before
     Eigen::VectorXd sol_bodies_global(bc_.get_global_solution_size());
     if (rank_ == 0)
