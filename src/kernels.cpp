d9b3320915d0fa75998c2089b936b17188af39d6
Robert Blackwell <rblackwell@flatironinstitute.org>
2022-08-09 11:13:18 -0400

    kernels: add openmp parallelism to kernels

--- src/kernels.cpp
+++ src/kernels.cpp
@@ -1,6 +1,7 @@
 #include <kernels.hpp>
 
 #include <STKFMM/STKFMM.hpp>
+#include <omp.h>
 
 /*********************************************************
  *                                                        *
@@ -43,16 +44,33 @@ Eigen::MatrixXd kernels::stokeslet_direct_cpu(MatrixRef &r_sl, MatrixRef &r_dl, 
                                               MatrixRef &f_dl, double eta) {
     Eigen::MatrixXd u_trg = Eigen::MatrixXd::Zero(3, r_trg.cols());
 
-    pvfmm::stokes_vel::Eval(const_cast<double *>(r_sl.data()), r_sl.cols(), const_cast<double *>(f_sl.data()), 1,
-                            const_cast<double *>(r_trg.data()), r_trg.cols(), u_trg.data(), &g_memmgr);
+#pragma omp parallel for schedule(static)
+    for (int i = 0; i < omp_get_num_threads(); ++i) {
+        auto n_trg = r_trg.cols() / omp_get_num_threads();
+        auto i_trg_0 = n_trg * i;
+        n_trg = std::min(n_trg, r_trg.cols() - i_trg_0);
+
+        pvfmm::stokes_vel::Eval(const_cast<double *>(r_sl.data()), r_sl.cols(), const_cast<double *>(f_sl.data()), 1,
+                                const_cast<double *>(r_trg.data() + i_trg_0 * 3), n_trg, u_trg.data() + i_trg_0 * 3,
+                                &g_memmgr);
+    }
     return u_trg / eta;
 }
 
 Eigen::MatrixXd kernels::stresslet_direct_cpu(MatrixRef &r_sl, MatrixRef &r_dl, MatrixRef &r_trg, MatrixRef &f_sl,
                                               MatrixRef &f_dl, double eta) {
     Eigen::MatrixXd u_trg = Eigen::MatrixXd::Zero(3, r_trg.cols());
-    stokes_doublevel::Eval(const_cast<double *>(r_dl.data()), r_dl.cols(), const_cast<double *>(f_dl.data()), 1,
-                           const_cast<double *>(r_trg.data()), r_trg.cols(), u_trg.data(), &g_memmgr);
+
+#pragma omp parallel for schedule(static)
+    for (int i = 0; i < omp_get_num_threads(); ++i) {
+        auto n_trg = r_trg.cols() / omp_get_num_threads();
+        auto i_trg_0 = n_trg * i;
+        n_trg = std::min(n_trg, r_trg.cols() - i_trg_0);
+
+        stokes_doublevel::Eval(const_cast<double *>(r_dl.data()), r_dl.cols(), const_cast<double *>(f_dl.data()), 1,
+                               const_cast<double *>(r_trg.data() + i_trg_0 * 3), n_trg, u_trg.data() + i_trg_0 * 3,
+                               &g_memmgr);
+    }
 
     return u_trg / eta;
 }
