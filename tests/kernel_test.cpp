bd49e6b65c111f8b1f31f14d510da8f3c12428bc
Robert Blackwell <rblackwell@flatironinstitute.org>
2022-10-13 08:12:25 -0400

    kernels: clean up tester a bit

--- tests/kernel_test.cpp
+++ tests/kernel_test.cpp
@@ -7,11 +7,11 @@ int main(int argc, char *argv[]) {
 
     using Eigen::MatrixXd;
 
-    constexpr int n_src = 1000;
-    constexpr int n_trg = 1000;
-    constexpr double eta = 1.0;
+    constexpr int n_src = 1229;
+    constexpr int n_trg = 743;
+    constexpr double eta = 1.3;
 
-    const int mult_order = 8;
+    const int mult_order = 12;
     const int max_pts = 50;
     auto stresslet_kernel_fmm = kernels::FMM<stkfmm::Stk3DFMM>(mult_order, max_pts, stkfmm::PAXIS::NONE,
                                                                stkfmm::KERNEL::PVel, kernels::stokes_pvel_fmm);
@@ -24,13 +24,12 @@ int main(int argc, char *argv[]) {
         MatrixXd nullmat;
         omp_set_num_threads(1);
         MatrixXd res_single = kernels::stokeslet_direct_cpu(r_src, nullmat, r_trg, f_src, nullmat, eta);
-        omp_set_num_threads(5);
+        omp_set_num_threads(6);
         MatrixXd res_openmp = kernels::stokeslet_direct_cpu(r_src, nullmat, r_trg, f_src, nullmat, eta);
         MatrixXd res_gpu = kernels::stokeslet_direct_gpu(r_src, nullmat, r_trg, f_src, nullmat, eta);
         MatrixXd res_fmm = stokeslet_kernel_fmm(r_src, nullmat, r_trg, f_src, nullmat, eta);
 
-        std::cout << (res_openmp - res_single).mean() << std::endl;
-        std::cout << (res_openmp - res_gpu).mean() << std::endl;
+        std::cout << (res_single - res_openmp).mean() << std::endl;
         std::cout << (res_single - res_gpu).mean() << std::endl;
         std::cout << (res_single - res_fmm).mean() << std::endl;
     }
@@ -42,13 +41,12 @@ int main(int argc, char *argv[]) {
 
         omp_set_num_threads(1);
         MatrixXd res_single = kernels::stresslet_direct_cpu(nullmat, r_src, r_trg, nullmat, f_src, eta);
-        omp_set_num_threads(5);
+        omp_set_num_threads(6);
         MatrixXd res_openmp = kernels::stresslet_direct_cpu(nullmat, r_src, r_trg, nullmat, f_src, eta);
         MatrixXd res_gpu = kernels::stresslet_direct_gpu(nullmat, r_src, r_trg, nullmat, f_src, eta);
         MatrixXd res_fmm = stresslet_kernel_fmm(nullmat, r_src, r_trg, nullmat, f_src, eta);
 
-        std::cout << (res_openmp - res_single).mean() << std::endl;
-        std::cout << (res_openmp - res_gpu).mean() << std::endl;
+        std::cout << (res_single - res_openmp).mean() << std::endl;
         std::cout << (res_single - res_gpu).mean() << std::endl;
         std::cout << (res_single - res_fmm).mean() << std::endl;
     }
