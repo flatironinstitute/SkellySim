a07eb67fba0a8d41f1b3ae0371a19348cad0ab6b
Robert Blackwell <rblackwell@flatironinstitute.org>
2023-05-24 11:04:38 -0400

    tests: update clamped_buckling tests

--- examples/stokes_tests/clamped_buckling/plot_deflection.py
+++ examples/stokes_tests/clamped_buckling/plot_deflection.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
 
 from skelly_sim.reader import TrajectoryReader
-import numpy as np
+
 import matplotlib
 matplotlib.use('TKAgg')
 import matplotlib.pyplot as plt
 
-traj = TrajectoryReader('skelly_config.toml')
+def get_deflection(path: str):
+    traj = TrajectoryReader(path)
 
-x = []
-for i in range(len(traj)):
-    traj.load_frame(i)
-    x.append(traj['fibers'][0]['x_'][-1, 0])
+    x = []
+    for i in range(len(traj)):
+        traj.load_frame(i)
+        x.append(traj['fibers'][0]['x_'][-1, 0])
 
-plt.plot(traj.times, x)
+    return traj.times, x
+
+t72, x72 = get_deflection('sigma72/skelly_config.toml')
+t80, x80 = get_deflection('sigma80/skelly_config.toml')
+
+plt.plot(t72, x72, t80, x80)
+plt.legend(['\u03c3 = 72', '\u03c3 = 80'])
 plt.xlabel('Time (s)')
 plt.ylabel('Deflection (micrometers)')
 plt.show()
--- examples/stokes_tests/clamped_buckling/sigma80/gen_config.py
+++ examples/stokes_tests/clamped_buckling/sigma80/gen_config.py
@@ -1,47 +1,45 @@
 #!/usr/bin/env python3
 
-import sys
 import numpy as np
-from skelly_sim.skelly_config import Config, Fiber, perturbed_fiber_positions
-
-config_file = 'skelly_config.toml'
-if len(sys.argv) == 1:
-    print("Using default toml file for output: 'skelly_config.toml'. "
-          "Provide an alternative filename argument to this script to use that instead.")
-elif len(sys.argv) != 2:
-    print("Supply output config path (blahblah.toml) as sole argument")
-    sys.exit()
-else:
-    config_file = sys.argv[1]
-
-np.random.seed(100)
+from skelly_sim.skelly_config import Config, Fiber, Point
 
 # create a config object and set the system parameters
 config = Config()
 config.params.eta = 1.0
-config.params.dt_initial = 1E-1
-config.params.dt_write = 1.0
-config.params.t_final = 10000.0
-config.params.gmres_tol = 1E-10
-config.params.seed = 130319
-config.params.adaptive_timestep_flag = False
+config.params.dt_initial = 0.02
+config.params.dt_max = 0.02
+config.params.dt_write = 0.1
+config.params.t_final = 200.0
+config.params.pair_evaluator = "CPU"
 
-sigma = 0.025
-length = 2.0
+sigma = 80.
+
+length = 1.0
 bending_rigidity = 0.0025
-n_nodes = 64
+force_scale = -sigma * bending_rigidity / length**3
+n_nodes = 32
 
-print(sigma * length**3 / bending_rigidity)
+config.fibers = [
+    Fiber(
+        force_scale=force_scale,
+        length=length,
+        n_nodes=n_nodes,
+        bending_rigidity=bending_rigidity,
+        minus_clamped=True,
+    )
+]
 
-config.fibers = [Fiber(
-    force_scale=-sigma,
-    length=length,
-    n_nodes=n_nodes,
-    bending_rigidity=bending_rigidity,
-    minus_clamped=True,
-)]
-x = perturbed_fiber_positions(0.01, length, np.array([0.0, 0.0, 0.0]), np.array([0.0, 0.0, 1.0]), n_nodes, np.array([1.0, 0.0, 0.0]))
-config.fibers[0].x = x.ravel().tolist()
+# give a small kick to start from
+config.point_sources = [
+    Point(
+        position=[0.0, 0.0, 10 * length],
+        force=[10.0, 0.0, 0.0],
+        time_to_live=1.0,
+    )
+]
+
+# orient in z
+config.fibers[0].x = np.linspace([0, 0, 0], [0, 0, length], n_nodes).ravel().tolist()
 
 # output our config
-config.save(config_file)
+config.save()
