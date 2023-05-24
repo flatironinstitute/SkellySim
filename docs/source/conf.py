1603bf4174833e842f01a594e02f98c55c3a01c8
Robert Blackwell <rblackwell@flatironinstitute.org>
2023-05-24 11:11:57 -0400

    Bump version to 0.9.12

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Robert Blackwell, Gokberk Kabacaoglu'
 author = 'Robert Blackwell, Gokberk Kabacaoglu'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.11'
+release = '0.9.12'
 
 
 # -- General configuration ---------------------------------------------------
