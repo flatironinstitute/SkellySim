0b2d0a9c6f2346fa1b617aa41571d49cd57e32b3
Robert Blackwell <rblackwell@flatironinstitute.org>
2023-10-12 10:10:23 -0400

    cleanup: move barycentric_matrix to utils

--- src/core/utils.cpp
+++ src/core/utils.cpp
@@ -1,7 +1,40 @@
 #include <skelly_sim.hpp>
+#include <limits>
+
 #include <utils.hpp>
 
 #include <cnpy.hpp>
+
+/// @brief Return resampling matrix P_{N,-m}.
+/// @param[in] x [N] vector of points x_k.
+/// @param[in] y [N-m] vector
+/// @return Resampling matrix P_{N, -m}
+Eigen::MatrixXd utils::barycentric_matrix(ArrayRef &x, ArrayRef &y) {
+    int N = x.size();
+    int M = y.size();
+
+    Eigen::ArrayXd w = Eigen::ArrayXd::Ones(N);
+    for (int i = 1; i < N; i += 2)
+        w(i) = -1.0;
+    w(0) = 0.5;
+    w(N - 1) = -0.5 * std::pow(-1, N);
+
+    Eigen::MatrixXd P = Eigen::MatrixXd::Zero(M, N);
+    for (int j = 0; j < M; ++j) {
+        double S = 0.0;
+        for (int k = 0; k < N; ++k) {
+            S += w(k) / (y(j) - x(k));
+        }
+        for (int k = 0; k < N; ++k) {
+            if (std::fabs(y(j) - x(k)) > std::numeric_limits<double>::epsilon())
+                P(j, k) = w(k) / (y(j) - x(k)) / S;
+            else
+                P(j, k) = 1.0;
+        }
+    }
+    return P;
+}
+
 
 //  Following the paper Calculation of weights in finite different formulas,
 //  Bengt Fornberg, SIAM Rev. 40 (3), 685 (1998).
