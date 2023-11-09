ef08f52d537bcb32663ced3e6f1afa6b4af84027
Christopher Edelmaier <cedelmaier@flatironinstitute.org>
2023-11-09 16:09:28 -0500

    core: working on making ellipsoids are consistent with spheres, which now they are

--- tests/combined/bodies/test_ellipsoid_prolate_z.py
+++ tests/combined/bodies/test_ellipsoid_prolate_z.py
@@ -6,7 +6,7 @@ from skelly_sim.skelly_config import Config, Body
 from skelly_sim.testing import working_directory, run_sim, run_precompute, sim_path
 
 np.random.seed(100)
-n_nodes = 2000
+n_nodes = 800
 config_file = 'skelly_config.toml'
 
 def gen_config(path: Path=('.')):
@@ -18,16 +18,23 @@ def gen_config(path: Path=('.')):
     config.params.dt_min = 1E-4
     config.params.dt_max = 1E-1
     config.params.dt_write = 1E-1
-    config.params.t_final = 1.0
+    config.params.t_final = 1.5
     config.params.gmres_tol = 1E-10
     config.params.seed = 130319
     config.params.pair_evaluator = "CPU"
+
+    # Generate an axis length for a,b that is slightly smaller than before
+    smalleps = 0.1
+    radius = 0.5
+    body_a = radius * (1.0 - smalleps)
+    body_b = radius * (1.0 - smalleps)
+    body_c = radius
 
     config.bodies = [
         Body(n_nucleation_sites=0,
              position=[0.0, 0.0, 0.0],
              shape='ellipsoid',
-             axis_length=[0.5, 0.5, 0.5],
+             axis_length=[body_a, body_b, body_c],
              n_nodes=n_nodes,
              external_force=[0.0, 0.0, 1.5])
     ]
@@ -51,25 +58,34 @@ def velocity(path: Path=Path('.')):
         # We need the hydrodynamic radius, which is slightly different than the supplied 'attachment'
         # radius. let's just grab it from the precompute data.
         precompute_data = np.load(traj.config_data['bodies'][0]['precompute_file'])
-        # Find the max radius in XY to use for the hydrodynamic radius (for now)
-        xmax = np.max(precompute_data["node_positions_ref"][:,0])
-        xmin = np.min(precompute_data["node_positions_ref"][:,0])
-        ymax = np.max(precompute_data["node_positions_ref"][:,1])
-        ymin = np.min(precompute_data["node_positions_ref"][:,1])
-        dxr = (xmax - xmin) / (2.0)
-        dyr = (ymax - ymin) / (2.0)
-        radius = (dxr + dyr) / (2.0)
-        print("Effective radius XY: {}".format(radius))
+        # Compute all the radii for the ellipsoid, then extract the two axes
+        radii = np.zeros(len(precompute_data['node_positions_ref']))
+        for idx in range(len(radii)):
+            radii[idx] = np.linalg.norm(precompute_data['node_positions_ref'][idx,:])
+
+        a = np.max(radii)
+        b = np.min(radii)
+        # Derive back smalleps due to changes in the hydrodynamic radius that occur under the hood
+        print("Effective radius a:      {}".format(a))
+        print("Effective radius b:      {}".format(b))
+        smalleps = 1.0 - b/a
+        eccentricity = np.sqrt(1.0 - (b/a)**2)
+        print("Effective epsilon:       {}".format(smalleps))
+        print("Effective eccentricity:  {}".format(eccentricity))
         eta = traj.config_data['params']['eta']
         force = traj.config_data['bodies'][0]['external_force'][2]
 
-        v_theoretical = force / (6 * np.pi * eta * radius)
+        v_parallel = force / (16.0 * np.pi * eta * a * np.power(eccentricity,3) / ((1 + np.power(eccentricity,2))*np.log((1+eccentricity)/(1-eccentricity))-2*eccentricity))
+        v_theoretical_sphere_large = force / (6.0 * np.pi * eta * a)
+        v_theoretical_sphere_small = force / (6.0 * np.pi * eta * b)
         v_measured = (z_final - z_initial) / dt
-        error = abs(1 - v_measured / v_theoretical)
+        error = abs(1 - v_measured / v_parallel)
 
-        print("Measured velocity:    {}".format(v_measured))
-        print("Theoretical velocity: {}".format(v_theoretical))
-        print("Error |1 - v/v0|:     {}".format(error))
+        print("Measured velocity:                   {}".format(v_measured))
+        print("Theoretical velocity (parallel):     {}".format(v_parallel))
+        print("Theoretical velocity (small sphere): {}".format(v_theoretical_sphere_small))
+        print("Theoretical velocity (large sphere): {}".format(v_theoretical_sphere_large))
+        print("Error |1 - v/v0|:                    {}".format(error))
 
         return error
 
